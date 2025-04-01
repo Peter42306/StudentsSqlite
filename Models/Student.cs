867f56af9d549f42f1a3500d0d36c3876dd78e08
Petr Zalizko <pzalizko@gmail.com>
2025-04-01 21:23:01 +0300

    Initial commit

---
+++ Models/Student.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentsSqlite.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, ErrorMessage = "Not more than 50 symbols")]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage = "Wrong email format")]
+        [StringLength(50)]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [RegularExpression(@"^\+380\d{9}$", ErrorMessage = "Format: +380XXXXXXXXX")]
+        [Display(Name = "Phone")]
+        public string Phone { get; set; }
+    }
+}
